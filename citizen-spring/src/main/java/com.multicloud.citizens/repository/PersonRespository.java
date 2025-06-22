25bbf55169a4d6d94dc0407b8a03031124471e7a
sdprog771 <sdprog771@gmail.com>
2025-06-22 21:26:08 +0300

    Person model, repository, controller created.

---
+++ citizen-spring/src/main/java/com.multicloud.citizens/repository/PersonRespository.java
@@ -0,0 +1,14 @@
+package com.multicloud.citizens.repository;
+
+import com.multicloud.citizens.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PersonRespository extends JpaRepository<Person, Long> {
+    List<Person> findByFields(Long at, String firstName, String lastName, String gender, LocalDateTime birthDate, Long afm, String homeAddress);
+}
