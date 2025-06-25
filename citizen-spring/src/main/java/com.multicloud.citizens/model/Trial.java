fe740959ea17196569dc9bcc37627321128d0866
sdprog771 <sdprog771@gmail.com>
2025-06-25 14:28:57 +0300

    test 2

--- citizen-spring/src/main/java/com.multicloud.citizens/model/Trial.java
+++ citizen-spring/src/main/java/com.multicloud.citizens/model/Trial.java
@@ -3,8 +3,10 @@ package com.multicloud.citizens.model;
 public class Trial {
 
     private String name;
+    private String surname;
 
-    public Trial(String name) {
+    public Trial(String name, String surname) {
         this.name = name;
+        this.surname = surname;
     }
 }
