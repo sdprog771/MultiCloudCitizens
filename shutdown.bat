25bd798aa8089490ac78e9c55a44fc48533aee66
sdprog771 <sdprog771@gmail.com>
2025-06-25 11:18:36 +0300

    refactor stop

---
+++ shutdown.bat
@@ -0,0 +1,4 @@
+docker container stop mysql person
+docker container rm mysql person
+docker network rm person-net
+docker system prune -f
---
+++ shutdown.sh
@@ -0,0 +1,4 @@
+docker container stop mysql person
+docker container rm mysql person
+docker network rm book-person
+docker system prune -f
